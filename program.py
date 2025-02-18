79a15e2620abab8ef50820600a4653826c56d97a
lebedev <kilbdv@yandex.ru>
2025-02-18 15:49:55 +0500

    task 2

--- program.py
+++ program.py
@@ -7,7 +7,15 @@ from yandex_map_library import *
 class MapApp:
     def __init__(self, size):
         self.screen = pygame.display.set_mode(size)
-        bytes_img = get_static_map(ll='123,45', z=5)
+        self.z = 10
+        self.coord = [123, 45]
+        self.update_map()
+
+    def update_map(self):
+        bytes_img = get_static_map(
+            ll=','.join(map(str, self.coord)),
+            z=self.z
+        )
         img = pygame.image.load(io.BytesIO(bytes_img))
         self.screen.blit(img, (0, 0))
 
@@ -17,6 +25,12 @@ class MapApp:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit(0)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_PAGEUP:
+                        self.z = min(21, self.z + 1)
+                    if event.key == pygame.K_PAGEDOWN:
+                        self.z = max(0, self.z - 1)
+                    self.update_map()
 
             pygame.display.flip()
 
