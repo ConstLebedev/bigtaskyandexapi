7ed4177e69a5d1ddc7595b870735043e6c559990
lebedev <kilbdv@yandex.ru>
2025-02-18 16:57:25 +0500

    task 4 pre

--- program.py
+++ program.py
@@ -1,48 +1,87 @@
 import io
 import pygame
+import pygame_gui
 
 from yandex_map_library import *
+
+
+class GUI:
+    def __init__(self, size, theme):
+        self.manager = pygame_gui.UIManager(size, )
+        self.theme_toggle = pygame_gui.elements.UIDropDownMenu(
+            MapApp.THEMES,
+            MapApp.THEMES[theme],
+            pygame.rect.Rect(10, 10, 100, 30),
+            self.manager,
+        )
+
+    def update(self, td):
+        self.manager.update(td)
+
+    def draw(self, surf):
+        self.manager.draw_ui(surf)
+
+    def process_event(self,event):
+        self.manager.process_events(event)
+        if event.type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+            if event.ui_element == self.theme_toggle:
+                print('выбираем тему')
 
 
 class MapApp:
     DELTA_LON = 200
     DELTA_LAT = 90
+    THEMES = 'light', 'dark'
 
     def __init__(self, size):
+        pygame.init()
         self.screen = pygame.display.set_mode(size)
         self.z = 1
         self.coord = [0, 0]
+        self.theme = 0
+        self.clock = pygame.time.Clock()
         self.update_map()
+        self.gui = GUI(size, self.theme)
 
     def update_map(self):
         bytes_img = get_static_map(
             ll=','.join(map(str, self.coord)),
-            z=self.z
+            z=self.z,
+            theme=MapApp.THEMES[self.theme]
         )
         img = pygame.image.load(io.BytesIO(bytes_img))
         self.screen.blit(img, (0, 0))
 
+    def process_keys(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                self.z = min(20, self.z + 1)
+            if event.key == pygame.K_PAGEDOWN:
+                self.z = max(-1, self.z - 1)
+            if event.key == pygame.K_LEFT:
+                self.coord[-1] = (self.coord[0] - MapApp.DELTA_LON * 2 ** -self.z + 180) % 360 - 180
+            if event.key == pygame.K_RIGHT:
+                self.coord[-1] = (self.coord[0] + MapApp.DELTA_LON * 2 ** -self.z + 180) % 360 - 180
+            if event.key == pygame.K_UP:
+                self.coord[0] = min((self.coord[1] + MapApp.DELTA_LAT * 2 ** -self.z), 85)
+            if event.key == pygame.K_DOWN:
+                self.coord[0] = max((self.coord[1] - MapApp.DELTA_LAT * 2 ** -self.z), -85)
+            if event.key == pygame.K_t:
+                self.theme = 0 - self.theme
+            self.update_map()
+
     def run(self):
         while True:
+            time_delta = self.clock.tick(60) / 1000.0
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit(0)
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_PAGEUP:
-                        self.z = min(21, self.z + 1)
-                    if event.key == pygame.K_PAGEDOWN:
-                        self.z = max(0, self.z - 1)
-                    if event.key == pygame.K_LEFT:
-                        self.coord[0] = (self.coord[0] - MapApp.DELTA_LON * 2 ** -self.z + 180) % 360 - 180
-                    if event.key == pygame.K_RIGHT:
-                        self.coord[0] = (self.coord[0] + MapApp.DELTA_LON * 2 ** -self.z + 180) % 360 - 180
-                    if event.key == pygame.K_UP:
-                        self.coord[1] = min((self.coord[1] + MapApp.DELTA_LAT * 2 ** -self.z), 85)
-                    if event.key == pygame.K_DOWN:
-                        self.coord[1] = max((self.coord[1] - MapApp.DELTA_LAT * 2 ** -self.z), -85)
-                    self.update_map()
+                self.gui.process_event(event)
+                self.process_keys(event)
 
+            self.gui.update(time_delta)
+            self.gui.draw(self.screen)
             pygame.display.flip()
 
 
